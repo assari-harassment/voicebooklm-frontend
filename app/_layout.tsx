60322a004ef6edfc71dd4bf95c08f76192204691
Fuku-x <kd1373132@st.kobedenshi.ac.jp>
2026-01-06 11:04:06 +0900

    feat: SafeAreaProviderを追加 #56

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,14 +1,17 @@
-import '../global.css';
 import { AuthProvider } from '@/src/shared/providers/AuthProvider';
 import { Stack } from 'expo-router';
 import { PaperProvider } from 'react-native-paper';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
+import '../global.css';
 
 export default function RootLayout() {
   return (
-    <PaperProvider>
-      <AuthProvider>
-        <Stack screenOptions={{ headerShown: false }} />
-      </AuthProvider>
-    </PaperProvider>
+    <SafeAreaProvider>
+      <PaperProvider>
+        <AuthProvider>
+          <Stack screenOptions={{ headerShown: false }} />
+        </AuthProvider>
+      </PaperProvider>
+    </SafeAreaProvider>
   );
 }
