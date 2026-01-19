8167aeb51db6a10543846badb798102309856459
JunichiHattori <junhat6@gmail.com>
2026-01-19 22:03:25 +0900

    検索履歴関連の機能を削除し、SearchScreenの初期状態を更新 #113

--- src/features/search/index.ts
+++ src/features/search/index.ts
@@ -1,6 +1,3 @@
 export { PopularTagChip, PopularTags, usePopularTags } from './popular-tags';
-export { SearchHistory } from './SearchHistory';
-export { SearchHistoryItem } from './SearchHistoryItem';
 export { SearchScreen } from './SearchScreen';
-export { useSearchHistory } from './useSearchHistory';
 export { useSearchMemos } from './useSearchMemos';
